91d1fbb18409d56993dfe3cfc5127c2c92651004
derrickdso <derrickjdieso@gmail.com>
2021-07-01 13:30:10 -0400

    gatekeeper

--- src/App.vue
+++ src/App.vue
@@ -236,8 +236,7 @@ Vue.use(VueFuse);
 // const endpoint =
 //   "https://cors-anywhere.herokuapp.com/phila.gov/wp-json/the-latest/v1/";
 
-const endpoint =
-  "https://www.phila.gov/wp-json/the-latest/v1/";
+const endpoint = "https://api.phila.gov/the-latest/v1/";
 
 export default {
   name: "Archives",
